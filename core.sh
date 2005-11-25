2809723afd2c9e37b66b8775aed4de04ac8d761c
Mike McLean <mikem@fedoraproject.org>
2005-11-25 22:16:48 +0000

    sync

--- core.sh
+++ core.sh
@@ -28,23 +28,8 @@ assign () {
 __bdsm_index=0
 __bdsm_maxindex=2147483647
 
-
-foo () {
-    local x=$1
-    let x=x+1
-    if (( x < 32 )); then
-        foo $x
-        echo ": $REPLY"
-    fi
-    echo $x
-    REPLY=$x
-}
-
-
 alloc () {
-    local valtype=$1
-    local value=$2
-
+    ### return an unused global name for bdsm data
     #increment the index, which is a *list* of numbers.
     #we increment with overflow.
     local incr=1
@@ -63,40 +48,119 @@ alloc () {
     if ((incr > 0)); then
         newindex="$newindex 0"
     fi
-
     __bdsm_index=$newindex
-    REPLY=__bdsm_data_$valtype
+    #now that we have the new index, generate the global name
+    REPLY=__bdsm_data
     for i in ${__bdsm_index}; do
         REPLY="${REPLY}_$i"
     done
-    eval "$REPLY=\$value"
 }
 
 
-str () {
-    ### return a new string object
-    alloc str "$1"
-}
-
-int () {
-    ### return a new int object
-    alloc int "$1"
-}
-
-whattype () {
-    ### return the type of the given data
-    local t
-    case $1 in
-        __bdsm_data_*)
-            t=${1#__bdsm_data_}
-            t=${t%%_*}
-            alloc str "$t"
+object () {
+    local loc=$1
+    local cmd=$2
+    shift
+    shift
+    case $cmd in
+        class)
+            # XXX - use ${!var} expansion?
+            eval "REPLY=\$${loc}_class"
             return
             ;;
-        *)
-            return 1
+        new)
+            alloc
+            eval "${REPLY}_class=object"
+            return
+            ;;
+        getattr)
+            #attr name in $1
+            eval "REPLY=\$${loc}_attr_$1"
+            return
+            ;;
+        setattr)
+            #value in $1
+            eval "${loc}_attr_$1=\$1"
+            unset -v REPLY
+            return
             ;;
     esac
 }
+
+
+getclass () {
+    ### return the class of the given object
+    object "$1" class
+}
+
+getattr () {
+    ### return the value of the attribute for an object
+    object "$1" class
+    #RETURN holds the name of the class function
+    "$RETURN" "$1" getattr "$2"
+}
+
+setattr () {
+    ### set the attribute value for an object
+    object "$1" class
+    #RETURN holds the name of the class function
+    "$RETURN" "$1" setattr "$2"
+}
+
+native () {
+    ### native class (for bash native scalars)
+    local loc=$1
+    local cmd=$2
+    local parent=object
+    shift
+    shift
+    case $cmd in
+        new)
+            alloc
+            eval "${REPLY}_class=native"
+            eval "${REPLY}=\$1"
+            return
+            ;;
+        set)
+            eval "${loc}=\$1"
+            unset -v REPLY
+            return
+            ;;
+        value)
+            eval "REPLY=\$${loc}"
+            return
+            ;;
+        *)
+            #defer to parent
+            $parent $loc $cmd "$@"
+            return
+            ;;
+    esac
+}
+
+string () {
+    ### string class
+    local loc=$1
+    local cmd=$2
+    local parent=native
+    shift
+    shift
+    case $cmd in
+        new)
+            alloc
+            eval "${REPLY}_class=native"
+            #initialize value from $1
+            eval "${REPLY}=\$1"
+            return
+            ;;
+        *)
+            #defer to parent
+            $parent $loc $cmd "$@"
+            return
+            ;;
+    esac
+}
+
+
 
 
